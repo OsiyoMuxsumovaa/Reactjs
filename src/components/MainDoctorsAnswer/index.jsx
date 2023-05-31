c1d8bd23a3578039054e7ba0686e5df3c2eec6ab
Khojiakbar Mukhamedov <osiyomuhsumova@gmail.com>
2023-05-31 10:37:23 +0500

    textini yozdim

--- src/components/MainDoctorsAnswer/index.jsx
+++ src/components/MainDoctorsAnswer/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./answers.css";
+import answerService from '../MainDoctorsAnswer/answerService.png'
 
 const MainDoctorsAnswer = () => {
   return (
-    <div className="dr-backround">
-    
+    <div className="doc-answer">
+     <h1>Post your questions <br/> our experts are ready to <br/> answer</h1>
+     <p>We service to you 24/7 with ensure 100% care</p>
+     <li>Development Facebook incubator stealth </li>
+     <li>Development Facebook incubator stealth churn </li>
+     <li>Development Facebook incubator stealth </li>
+     <img src={answerService} alt="jkl" />
+     <button>Book an Appointment</button>
     </div>
   );
 };
