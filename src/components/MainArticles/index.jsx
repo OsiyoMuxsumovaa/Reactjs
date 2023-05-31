b373c246ccbb225af6d4f8294d498ae7bdbc6b75
Khojiakbar Mukhamedov <osiyomuhsumova@gmail.com>
2023-05-31 14:25:33 +0500

    yengi component ochdim

---
+++ src/components/MainArticles/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./articles.css";
+// import Avatar from "../MainArticles/docImg";
+// import Avatar1 from "../MainArticles/docImg1";
+// import Avatar2 from "../MainArticles/docImg2";
+
+
+const MainArticles = () => {
+  return (
+    <div className="backround-articles">
+     <p>ARTICLES & NEWS</p>
+     <h1>Our articles on health nd <br/> personal wellness </h1>
+     {/* <div className="cards">
+        
+        <div className="card">
+            <img src="docImg.png" alt="Avatar"  />
+            <div className="container">
+                <h1><b>ARTICLES</b></h1>
+                <p>Should you be tested post <br/> covid?</p>
+            </div>
+        </div>
+        
+        <div className="card1">
+        <img src="docImg1.png" alt="Avatar" />
+            <div className="container1">
+                <h1><b>ARTICLES</b></h1>
+                <p>Should you be tested post <br/> covid?</p>
+            </div>
+        </div>
+        
+        <div className="card2">
+        <img src="docImg2.png" alt="Avatar" />
+            <div className="container2">
+                <h1><b>ARTICLES</b></h1>
+                <p>Should you be tested post <br/> covid?</p>
+            </div>
+        </div>
+     </div> */}
+    </div>
+  );
+};
+
+export default MainArticles;
