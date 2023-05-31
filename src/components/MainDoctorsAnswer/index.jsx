92f192ea937918840e6faf25650d6258607223db
Khojiakbar Mukhamedov <osiyomuhsumova@gmail.com>
2023-05-31 13:24:29 +0500

    div ochib yengi button qoshdim

--- src/components/MainDoctorsAnswer/index.jsx
+++ src/components/MainDoctorsAnswer/index.jsx
@@ -7,11 +7,20 @@ const MainDoctorsAnswer = () => {
     <div className="doc-answer">
      <h1>Post your questions <br/> our experts are ready to <br/> answer</h1>
      <p>We service to you 24/7 with ensure 100% care</p>
+     <div className="li_line">
      <li>Development Facebook incubator stealth </li>
      <li>Development Facebook incubator stealth churn </li>
      <li>Development Facebook incubator stealth </li>
+     </div>
+     <div className="answerslogo">
      <img src={answerService} alt="jkl" />
-     <button>Book an Appointment</button>
+     </div>
+     <button className="button">Book an Appointment</button>
+     
+     <div className="doc-answerr">
+        <h1>Schedule an imperson or <br/>virtual  appointment today </h1>
+        <button className="buttonn">Book an Appointment</button>
+     </div>
     </div>
   );
 };
