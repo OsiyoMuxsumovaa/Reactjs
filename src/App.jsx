e5e4e586b7a2172feadf9add691a58e9e7eb9fba
Khojiakbar Mukhamedov <osiyomuhsumova@gmail.com>
2023-06-21 16:32:54 +0500

    renamed / to /home

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/company" element={<Company />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contacts" element={<Contacts />} />
